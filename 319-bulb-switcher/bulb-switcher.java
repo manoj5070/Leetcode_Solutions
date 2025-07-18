6f0fcc9629cee68f266ce1119e379af1210ee0fc
Manoj Kumar <baghelm665@gmail.com>
2025-07-18 11:40:04 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (47.46%) - LeetSync

---
+++ 319-bulb-switcher/bulb-switcher.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int bulbSwitch(int n) {
+        if(n==0) return n;
+        if(n<4) return 1;
+        // int count=0;
+        // for(int i=2;i<=(int)Math.sqrt(n);i++){
+        //     return 
+        // }
+        return (int)Math.sqrt(n);
+    }
+}
