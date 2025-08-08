d4878bdeabb574b96bbd9945207bbd26fc21b216
Manoj Kumar <baghelm665@gmail.com>
2025-08-08 11:20:20 +0530

    Time: 3 ms (34.75%) | Memory: 43.5 MB (17.77%) - LeetSync

--- 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.java
+++ 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.java
@@ -4,7 +4,11 @@ class Solution {
        int n=s.length();
 
        for(int i=0;i<n-2;i++){
-        if(s.charAt(i)!=s.charAt(i+1) && s.charAt(i)!=s.charAt(i+2) && s.charAt(i+2)!=s.charAt(i+1)) c++;
+        Set<Character> st=new HashSet<>();
+        st.add(s.charAt(i));
+        st.add(s.charAt(i+1));
+        st.add(s.charAt(i+2));
+        if(st.size()==3) c++;
        } 
        return c;
     }
