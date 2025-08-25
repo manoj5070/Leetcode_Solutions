0aa09919e0aedd78fa238a9801936307c89c181f
Manoj Kumar <baghelm665@gmail.com>
2025-08-25 21:30:20 +0530

    Time: 3 ms (77.98%) | Memory: 57.2 MB (28.24%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.java
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.java
@@ -1,37 +1,20 @@
 class Solution {
     public int longestSubarray(int[] nums) {
-        int max = 0;
-        int j = 0;
-        int index = -1;  
-        int n = nums.length;
-        boolean check = false;
-
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == 1) {
-                // \U0001f525 Fix: add (check ? 0 : 1)
-                max = Math.max(max, i - j + (check ? 0 : 1));
-            } else if (i < n - 1 && nums[i + 1] == 1) {
-                if (check) {
-                    j = index + 1;
-                }
-                index = i;           
-                check = true;
-            } else {
-                j = i + 1;
-                check = false;
+        int i=0;
+        int j=0;
+        int max=-1;
+        int zeros=0;
+        while(j<nums.length){
+            if(nums[j]==0) zeros++;
+            while(zeros>1){
+                if(nums[i]==0) zeros--;
+                i++;
             }
+            if(zeros>0){
+                max=Math.max(max,j-i);
+            }else max=Math.max(max,j-i+1);
+            j++;
         }
-
-        // no need to change your allOnes check
-        boolean allOnes = true;
-        for (int num : nums) {
-            if (num == 0) {
-                allOnes = false;
-                break;
-            }
-        }
-
-        if (allOnes) return n - 1; 
-        return max;
+        return max==nums.length?max-1:max;
     }
 }
