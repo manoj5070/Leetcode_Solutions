04ac5a22331f6675a8e3389de15e9676ca010510
Manoj Kumar <baghelm665@gmail.com>
2025-09-27 16:26:01 +0530

    Time: 20 ms (36.97%) | Memory: 17.8 MB (52.68%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -7,11 +7,13 @@ class Solution:
         j=0
         for i in range(len(s)):
             ch=s[i]
-            while(ch in map):
-                map.remove(s[j])
-                j+=1
-            map.add(ch)
-            ans=max(ans,len(map))
+            if(ch in map):
+                ans=max(ans,len(map))
+                while(ch in map):
+                    map.remove(s[j])
+                    j+=1
+                map.add(ch)
+            else:
+                map.add(ch)
+                ans=max(ans,len(map))
         return ans
-
-        
