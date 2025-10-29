28abbff8b82dfc806031537046cd7bdedd1b283f
Manoj Kumar <baghelm665@gmail.com>
2025-10-29 22:02:27 +0530

    Time: 3 ms (16.30%) | Memory: 17.7 MB (75.09%) - LeetSync

--- 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.py
+++ 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.py
@@ -1,11 +1,6 @@
-class Solution(object):
-    def smallestNumber(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
+class Solution:
+    def smallestNumber(self, n: int) -> int:
         x=n
         while((x&(x+1))!=0):
             x+=1
         return x
-        
