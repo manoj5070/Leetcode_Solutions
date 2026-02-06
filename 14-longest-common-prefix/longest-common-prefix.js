149200d31def79375e4819b0e35a5440bd93d550
Manoj Kumar <baghelm665@gmail.com>
2026-02-06 16:45:23 +0530

    Time: 1 ms (58.68%) | Memory: 53.6 MB (88.58%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.js
+++ 14-longest-common-prefix/longest-common-prefix.js
@@ -7,11 +7,8 @@ var longestCommonPrefix = function (strs) {
  let ans="";
  for(let i=0;i<n;i++){
     let ch=strs[0][i];
-        if(strs[i]=="") return "";
-        console.log(ch);
+    // if(strs[i]=="") return "";
     for(let j=1;j<strs.length;j++){
-        if(strs[j-1]=="") return "";
-        console.log(ch,strs[j][i]);
         if(ch!=strs[j][i]) return ans;
     }
         ans+=ch;
